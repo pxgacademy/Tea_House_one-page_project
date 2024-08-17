775e02f9333d3319427c36339661271d757b0d6f
Abdullah <graphicdesigner.abdullah@gmail.com>
2024-08-18 01:55:36 +0600

    Banner section completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+// Tailwindcss cdn
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        crimson: "#dc143c",
+        deeppink: "#ff1493",
+      },
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
+      },
+    },
+  },
+};
+
 // "use strict";
